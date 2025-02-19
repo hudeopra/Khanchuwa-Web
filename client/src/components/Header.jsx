3a4f696708b53a388ec9728adddd29e9db7d71c3
maharjanhudeo <maharjanhudeo@gmail.com>
2025-02-19 15:02:16 +0545

    header Component created

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,18 +1,14 @@
 import { Link, useNavigate } from "react-router-dom";
-import { FaSearch, FaShoppingCart, FaClipboardList } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
 import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
-
-  // //Checking the current user data
-  // useEffect(() => {
-  //   console.log("Current User Data:", currentUser);
-  // }, [currentUser]);
-
   const [searchTerm, setSearchTerm] = useState("");
   const navigate = useNavigate();
+  const [isMenuActive, setIsMenuActive] = useState(false);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const urlParams = new URLSearchParams(window.location.search);
@@ -22,58 +18,213 @@ export default function Header() {
   };
 
   useEffect(() => {
-    const urlParams = new URLSearchParams(location.search);
+    const urlParams = new URLSearchParams(window.location.search);
     const searchTermFromUrl = urlParams.get("searchTerm");
     if (searchTermFromUrl) {
       setSearchTerm(searchTermFromUrl);
     }
-  }, [location.search]);
+  }, [window.location.search]);
 
-  const user = currentUser?.user || currentUser;
-  const avatar = user?.avatar;
-  const username = user?.username;
+  const toggleMenu = () => {
+    setIsMenuActive(!isMenuActive);
+  };
 
   return (
-    <header className="kh-header bg-slate-200">
-      <div className="container flex justify-between">
-        <div className="kh-header__logo">
-          <Link to="/">
-            <img src="" alt="Main Logo" />
-          </Link>
-        </div>
-        <div className="kh-header__hmenu">
-          <nav>
-            <ul className="flex justify-around">
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/profile">
-                  {user ? (
-                    <div>
+    <header className="kh-header">
+      <div className="container-fluid">
+        <div className="kh-header__wrapper">
+          <div className="kh-header__head">
+            <div className="kh-header__head--nav">
+              <div className="trigger" onClick={toggleMenu}>
+                =
+              </div>
+              <nav
+                className={`kh-header__head--menu-wrapper ${
+                  isMenuActive ? "active" : ""
+                }`}
+              >
+                <div className="kh-header__head--menu-block">
+                  <p></p>
+                  <ul>
+                    <li>
                       <img
-                        className="rounded-full h-7 w-7 object-cover"
-                        src={avatar}
-                        alt="profile"
-                        accept="image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8"
+                        src="../src/assets/img/search/chefLogo.png"
+                        alt="Khanchuwa Logo"
                       />
-                      <span>{username}</span>
-                    </div>
+                      <span>Home</span>
+                    </li>
+                    <li>
+                      <img
+                        src="../src/assets/img/search/chefLogo.png"
+                        alt="Khanchuwa Logo"
+                      />
+                      <span>Recipes</span>
+                    </li>
+                    <li>
+                      <img
+                        src="../src/assets/img/search/chefLogo.png"
+                        alt="Khanchuwa Logo"
+                      />
+                      <span>Trending</span>
+                    </li>
+                    <li>
+                      <img
+                        src="../src/assets/img/search/chefLogo.png"
+                        alt="Khanchuwa Logo"
+                      />
+                      <span>Random</span>
+                    </li>
+                    <li>
+                      <img
+                        src="../src/assets/img/search/chefLogo.png"
+                        alt="Khanchuwa Logo"
+                      />
+                      <span>Search</span>
+                    </li>
+                    <li>
+                      <img
+                        src="../src/assets/img/search/chefLogo.png"
+                        alt="Khanchuwa Logo"
+                      />
+                      <span>About Khanchuwa</span>
+                    </li>
+                  </ul>
+                </div>
+                <div className="kh-header__head--menu-block">
+                  <p>Account</p>
+                  {currentUser ? (
+                    <ul>
+                      <li>
+                        <img
+                          src="../src/assets/img/search/chefLogo.png"
+                          alt="Khanchuwa Logo"
+                        />
+                        <span>My Profile</span>
+                      </li>
+                      <li>
+                        <img
+                          src="../src/assets/img/search/chefLogo.png"
+                          alt="Khanchuwa Logo"
+                        />
+                        <span>My Recipe</span>
+                      </li>
+                      <li>
+                        <img
+                          src="../src/assets/img/search/chefLogo.png"
+                          alt="Khanchuwa Logo"
+                        />
+                        <span>Sign Out</span>
+                      </li>
+                    </ul>
                   ) : (
-                    <span className="">Sign In</span>
+                    <ul>
+                      <li>
+                        <img
+                          src="../src/assets/img/search/chefLogo.png"
+                          alt="Khanchuwa Logo"
+                        />
+                        <span>Create Account</span>
+                      </li>
+                      <li>
+                        <img
+                          src="../src/assets/img/search/chefLogo.png"
+                          alt="Khanchuwa Logo"
+                        />
+                        <span>Sign In</span>
+                      </li>
+                    </ul>
                   )}
+                </div>
+                <div className="kh-header__head--menu-block">
+                  <p>Connect</p>
+                  <ul>
+                    <li>
+                      <img
+                        src="../src/assets/img/search/chefLogo.png"
+                        alt="Khanchuwa Logo"
+                      />
+                      <span>What’s New</span>
+                    </li>
+                    <li>
+                      <img
+                        src="../src/assets/img/search/chefLogo.png"
+                        alt="Khanchuwa Logo"
+                      />
+                      <span>Contact Us</span>
+                    </li>
+                  </ul>
+                </div>
+                <div className="kh-header__head--menu-block">
+                  <p>Resources</p>
+                  <ul>
+                    <li>
+                      <img
+                        src="../src/assets/img/search/chefLogo.png"
+                        alt="Khanchuwa Logo"
+                      />
+                      <span>FAQ</span>
+                    </li>
+                    <li>
+                      <img
+                        src="../src/assets/img/search/chefLogo.png"
+                        alt="Khanchuwa Logo"
+                      />
+                      <span>Site Map</span>
+                    </li>
+                  </ul>
+                </div>
+              </nav>
+            </div>
+            <div className="kh-header__head--main-logo">
+              <Link to="/">
+                <img
+                  src="../src/assets/img/logoKhanchuwa.png"
+                  alt="Khanchuwa Logo"
+                />
+              </Link>
+            </div>
+            <div className="kh-header__head--page-title">Current Page</div>
+          </div>
+          <div className="kh-header__search">
+            <form
+              onSubmit={handleSubmit}
+              className="bg-slate-100 p-3 rounded-lg flex items-center"
+            >
+              <input
+                type="text"
+                placeholder="Search..."
+                className="bg-transparent focus:outline-none w-24 sm:w-64"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+              />
+              <button>
+                <FaSearch className="text-slate-600" />
+              </button>
+            </form>
+          </div>
+          <div className="kh-header__account">
+            {currentUser ? (
+              <Link to="/profile">
+                <img
+                  className="rounded-full h-7 w-7 object-cover"
+                  src={currentUser.avatar}
+                  alt="profile"
+                />
+              </Link>
+            ) : (
+              <>
+                <Link to="/signin" className="text-slate-700 hover:underline">
+                  Sign In
                 </Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-        <div className="icon flex">
-          <FaSearch />
-          <FaShoppingCart className="ml-4" />
-          <FaClipboardList className="ml-4" />
+                <Link
+                  to="/signup"
+                  className="text-slate-700 hover:underline ml-4"
+                >
+                  Sign Up
+                </Link>
+              </>
+            )}
+          </div>
         </div>
       </div>
     </header>
